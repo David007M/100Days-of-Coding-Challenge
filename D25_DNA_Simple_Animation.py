80f6dbcdb57511b9ea69688a5a3e8c8cd011d824
Dawuti MAIHEMUTIJIANG <0600004923@sanoh.com>
2023-03-13 12:17:31 +0900

    Renamed and Comments added

--- D25_DNA_Simple_Animation.py
+++ D25_DNA_Simple_Animation.py
@@ -3,6 +3,7 @@ import random, sys, time
 PAUSE = 0.15  # (!) Try changing this to 0.5 or 0.0.
 
 # These are the individual rows of the DNA animation:
+# if you wanted you can try your own design for the DNA Helix
 ROWS = [
     #123456789 <- Use this to measure the number of spaces:
     '         ##',  # Index 0 has no {}.
